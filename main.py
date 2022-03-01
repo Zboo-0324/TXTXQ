b6f074b342ad7a44fd3db8695fae71c69f56a02b
Zboo-0324 <1558153409@qq.com>
2022-03-01 22:42:14 +0800

    master

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 
-firstdir = 'D:/Desktop/ex/'  # 要复制文件所在路径
-tardir = 'D:/Desktop/'  # 想要复制到的路径
+firstdir = 'D:/Desktop/ex/'  # 文件原路径
+tardir = 'D:/Desktop/'  # 文件目标路径路径
 pathdir = os.listdir(firstdir)  # 获取所在路径下的所有文件
 
 path = []
